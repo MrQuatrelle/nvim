fa32147a8b927d8074c0671300395d202c1742ce
MrQuatrelle <mrquatrelle@proton.me>
2024-12-18 11:23:38 +0000

    I collaborated in a repo with someone who uses Windows. You can guess the rest.

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,7 +4,7 @@ lualine.setup({
     options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = {
             statusline = {},
@@ -23,16 +23,16 @@ lualine.setup({
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { "filename" },
-        lualine_x = { "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" }
+        lualine_x = { "encoding", "fileformat" },
+        lualine_y = { "location" },
+        lualine_z = { "filetype" },
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { "filename" },
-        lualine_x = { "filetype" },
-        lualine_y = { "location" },
+        lualine_x = { "location" },
+        lualine_y = { "filetype" },
         lualine_z = {}
     },
     tabline = {},
