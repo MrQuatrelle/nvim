175b257efd6e4cbf8bd9519461500793812fbf15
MrQuatrelle <mrquatrelle@proton.me>
2025-03-25 09:02:54 +0000

    failed attempt 1

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,3 +1,23 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>c", ":RustLsp renderDiagnostic current<CR>")
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+    "n",
+    "<leader>a",
+    function()
+        -- supports rust-analyzer's grouping
+        vim.cmd.RustLsp('codeAction')
+    end,
+    { silent = true, buffer = bufnr }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>v",
+    function()
+        vim.cmd.RustLsp({ 'hover' })
+    end,
+    { silent = true, buffer = bufnr }
+)
