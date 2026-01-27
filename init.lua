ce6fa1c47d2dc1c292b5d480aa65304ec45de9ea
MrQuatrelle <mrquatrelle@proton.me>
2026-01-27 09:32:20 +0000

    (Neovide) hardcode "equal padding"

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 require("mrquatrelle")
 if vim.g.neovide then
     vim.o.guifont = "FiraCode Nerd Font:h15.5"
+
+    -- TODO: replace this with dynamic_padding, when they finally add it
+    vim.g.neovide_padding_top = 13
 end
